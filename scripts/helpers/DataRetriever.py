acee6eb554b2aadd9f1be713a67d5b32837d4e4a
piotrmilos <piotr.milos@codilime.com>
2017-05-26 12:18:06 +0200

    x

--- scripts/helpers/DataRetriever.py
+++ scripts/helpers/DataRetriever.py
@@ -47,60 +47,57 @@ def detect_host(e):
 def get_jobs_by_tags(tags):
     tags_list = tags.split(" ")
 
-    experiments = api.get_experiments(tags=tags_list)
+    experiments = api.get_experiments(tags=tags_list, states=["running"])
 
     experiments_data = []
 
-    for e in experiments:
-        if str(e.state) == 'running':
-            hierchical_specification_str = None
-            hierchical_specification_version = None
-            params = api.get_experiment_parameters(e.id)[0].to_dict()
-            for p in params['parameters']:
-                if p["name"] == "hierchical_specification_str":
-                    hierchical_specification_str = p["default_value"]
-                    hierchical_specification_version = 1
-                if p["name"] == "hierchical_specification_v2_str":
-                    hierchical_specification_str = p["default_value"]
-                    hierchical_specification_version = 2
+    for e in experiments.experiments:
+
+        hierchical_specification_str = None
+        hierchical_specification_version = None
+        params = e.best_job.parameters
+        for p in params:
+            if p.name == "hierchical_specification_str":
+                hierchical_specification_str = p["default_value"]
+                hierchical_specification_version = 1
+            if p.name == "hierchical_specification_v2_str":
+                hierchical_specification_str = p["default_value"]
+                hierchical_specification_version = 2
 
 
-            experiment_data = {}
-            experiment_data["name"] = e.name[34:]
-            url_id = api.get_experiment_jobs(e.id)[0].id
-            job = api.get_experiment_jobs(e.id)[0]
-            url = URL_PREFIX + url_id
-            experiment_data["_Neptune"] = url
-            experiment_data["id"] = e.id
-            host = detect_host(e)
-            movies_command = 'cd ~/TMP && rm -rf * && scp -r "{}:{}jobs/{}/src/dumpdir/*mp4" .'.format(host,
-                                                                          sh_escape(e.storage_location)[:-10],
-                                                                                 url_id)
-            experiment_data["_Movies"] = movies_command
-            if hierchical_specification_str is not None:
-                hierchical_specification_str = hierchical_specification_str.encode('ascii','ignore')[1:-1]
-                hierchical_specification = yaml.safe_load(str(hierchical_specification_str))
-                if hierchical_specification_version == 2:
-                    hierchical_specification.pop(0)
-                i = 0
-                for spec in hierchical_specification:
-                    if 'MacroPolicyFromTRPOmodularAgent' in spec[0][0]:
-                        file_spec = spec[0][1][0]
-                        if file_spec.startswith("file://"):
-                            experiment_data["_MacroTRPO:{}".format(i)] = file_spec[7:]
-                        if file_spec.startswith("neptune://"):
-                            neptune_job_id = file_spec[10:46]
-                            url = "https://ml.neptune.deepsense.io/#dashboard/job/{}".format(neptune_job_id)
-                            experiment_data["_MacroTRPO:{}".format(i)] = url
-                    if "MacroConstantAction" in spec[0][0]:
-                        experiment_data['_MacroConstant{}'.format(i)] = str(spec[0][1][0])
-                    i += 1
+        experiment_data = {}
+        experiment_data["name"] = e.name[34:]
+        url_id = api.get_experiment_jobs(e.id).jobs[0].id
+        # job = api.get_experiment_jobs(e.id)[0]
+        url = URL_PREFIX + url_id
+        experiment_data["_Neptune"] = url
+        experiment_data["id"] = e.id
+        host = detect_host(e)
+        movies_command = 'cd ~/TMP && rm -rf * && scp -r "{}:{}jobs/{}/src/dumpdir/*mp4" .'.format(host,
+                                                                      sh_escape(e.storage_location)[:-10],
+                                                                             url_id)
+        experiment_data["_Movies"] = movies_command
+        if hierchical_specification_str is not None:
+            hierchical_specification_str = hierchical_specification_str.encode('ascii','ignore')[1:-1]
+            hierchical_specification = yaml.safe_load(str(hierchical_specification_str))
+            if hierchical_specification_version == 2:
+                hierchical_specification.pop(0)
+            i = 0
+            for spec in hierchical_specification:
+                if 'MacroPolicyFromTRPOmodularAgent' in spec[0][0]:
+                    file_spec = spec[0][1][0]
+                    if file_spec.startswith("file://"):
+                        experiment_data["_MacroTRPO:{}".format(i)] = file_spec[7:]
+                    if file_spec.startswith("neptune://"):
+                        neptune_job_id = file_spec[10:46]
+                        url = "https://ml.neptune.deepsense.io/#dashboard/job/{}".format(neptune_job_id)
+                        experiment_data["_MacroTRPO:{}".format(i)] = url
+                if "MacroConstantAction" in spec[0][0]:
+                    experiment_data['_MacroConstant{}'.format(i)] = str(spec[0][1][0])
+                i += 1
 
 
-
-
-
-            experiments_data.append(experiment_data)
+        experiments_data.append(experiment_data)
 
     experiments_data = sorted(experiments_data, key=lambda x: x['name'])
     return experiments_data
@@ -108,11 +105,11 @@ def get_jobs_by_tags(tags):
 def abort_jobs_by_tags(tags):
     tags_list = tags.split(" ")
 
-    experiments = api.get_experiments(tags=tags_list)
+    experiments = api.get_experiments(tags=tags_list, states=["running"])
 
-    for e in experiments:
+    for e in experiments.experiments:
         if str(e.state) == 'running':
-            job = api.get_experiment_jobs(e.id)[0]
+            job = api.get_experiment_jobs(e.id).jobs[0]
             api.jobs_job_id_abort_post(job.id, x_neptune_user_role='normal_user')
 
     return "success"
